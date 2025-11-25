3687813526e408db952b5de36bfc6b37b1fbb70e
Kim-Dongdong <kdssos11@naver.com>
2025-11-25 22:23:13 +0900

    [Refactor] RedisConfig 및 application.yml 수정

--- src/main/java/kr/hhplus/be/server/config/jpa/RedissonConfig.java
+++ src/main/java/kr/hhplus/be/server/config/jpa/RedissonConfig.java
@@ -3,16 +3,38 @@ package kr.hhplus.be.server.config.jpa;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedissonConfig {
+
+	@Value("${spring.data.redis.host:localhost}")
+	private String redisHost;
+
+	@Value("${spring.data.redis.port:6379}")
+	private int redisPort;
+
 	@Bean
 	public RedissonClient redissonClient() {
 		Config config = new Config();
 		config.useSingleServer()
-			.setAddress("redis://localhost:6379");
+			.setAddress("redis://" + redisHost + ":" + redisPort);
 		return Redisson.create(config);
+	}
+
+	@Bean
+	public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+		RedisTemplate<String, String> template = new RedisTemplate<>();
+		template.setConnectionFactory(connectionFactory);
+		template.setKeySerializer(new StringRedisSerializer());
+		template.setValueSerializer(new StringRedisSerializer());
+		template.setHashKeySerializer(new StringRedisSerializer());
+		template.setHashValueSerializer(new StringRedisSerializer());
+		return template;
 	}
 }
